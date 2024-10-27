2c577c4b1e5add8d94f18bee8899382afe07d06c
stk346 <stk346@gmail.com>
2024-10-28 04:09:49 +0900

    init

---
+++ src/main/java/com/polar_moviechart/edgeservice/EdgesErviceApplication.java
@@ -0,0 +1,13 @@
+package com.polar_moviechart.edgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgesErviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgesErviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polar_moviechart/edgeservice/EdgesErviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polar_moviechart.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgesErviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
