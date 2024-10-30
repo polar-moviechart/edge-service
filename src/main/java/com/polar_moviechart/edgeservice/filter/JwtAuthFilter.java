1eb61e0506d447cc06b10c17612c27265c108607
stk346 <stk346@gmail.com>
2024-10-30 12:17:37 +0900

    feat: api 게이트웨이 mvc에서 jwt 관련 필터 설정 어려움으로 스프링 시큐리티로 jwt 관련 설정 적용

--- src/main/java/com/polar_moviechart/edgeservice/filter/JwtAuthFilter.java
+++ src/main/java/com/polar_moviechart/edgeservice/filter/JwtAuthFilter.java
@@ -5,9 +5,11 @@ import io.jsonwebtoken.Claims;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class JwtAuthFilter implements Filter {
 
     private final JwtTokenProcessor jwtTokenProcessor;
@@ -20,6 +22,14 @@ public class JwtAuthFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        // 특정 경로를 검사
+        String requestURI = httpRequest.getRequestURI();
+        if (requestURI.startsWith("/api/v1/movies") || requestURI.startsWith("/api/v1/login/kakao")) {
+            // 이 경로에서는 필터를 적용하지 않음
+            chain.doFilter(request, response);
+            return;
+        }
 
         String token = jwtTokenProcessor.extractToken(httpRequest);
         if (token == null) {
--- src/main/java/com/polar_moviechart/edgeservice/filter/JwtAuthFilterOptional.java
+++ src/main/java/com/polar_moviechart/edgeservice/filter/JwtAuthFilterOptional.java
@@ -3,9 +3,11 @@ package com.polar_moviechart.edgeservice.filter;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class JwtAuthFilterOptional implements Filter {
 
     private final JwtTokenProcessor jwtTokenProcessor;
@@ -16,8 +18,8 @@ public class JwtAuthFilterOptional implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
         HttpServletRequest httpRequest = (HttpServletRequest) request;
+
         String token = jwtTokenProcessor.extractToken(httpRequest);
 
         if (token != null) {
