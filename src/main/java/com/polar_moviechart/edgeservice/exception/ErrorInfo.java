1379f00da13f3f79a7bfd9eeebffefb25526718d
stk346 <stk346@gmail.com>
2024-11-06 07:16:51 +0900

    refactor: 커스텀 에러 응답 구조 변경. ErrorInfo를 사용해 에러메시지, 코드를 명시하도록 구조 변경

--- src/main/java/com/polar_moviechart/edgeservice/exception/ErrorInfo.java
+++ src/main/java/com/polar_moviechart/edgeservice/exception/ErrorInfo.java
@@ -3,21 +3,23 @@ package com.polar_moviechart.edgeservice.exception;
 import lombok.Getter;
 
 @Getter
-public enum ErrorCode {
+public enum ErrorInfo {
 
     DEFAULT_ERROR("9999", "예기치 못한 오류가 발생했습니다." + "\n" + "불편을 드려 죄송합니다."),
     TOKEN_CREATION_ERROR("100", "로그인 중 문제가 발생했습니다."),
-    NOT_ALLOWED("104", "접근 권한이 없습니다.");
+    NOT_ALLOWED("104", "접근 권한이 없습니다."),
+    TOKEN_EXPIRED("T101", "로그아웃 되었습니다." + "\n" + "다시 로그인해 주세요."),
+    TOKEN_NOT_EXISTS("T102", "토큰이 존재하지 않습니다.");
 
     private final String code;
     private final String message;
 
-    ErrorCode(String code, String message) {
+    ErrorInfo(String code, String message) {
         this.code = code;
         this.message = message;
     }
 
-    ErrorCode(String message) {
+    ErrorInfo(String message) {
         this.code = "1000";
         this.message = message;
     }
